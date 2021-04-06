0f7b2f49fbc161f4e7b6d987298bb5448475699c
sanskarkhare <sanskarkhare47@gmail.com>
2021-04-06 15:13:44 +0545

    update route

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -17,6 +17,32 @@ exports.getTransactions = async(req, res, next) => {
     }
 }
 
+exports.getTransactionsById = async(req, res, next) => {
+    try {
+        
+        const transaction = await Transaction.findById(req.params.id);
+
+        if(!transaction) {
+            return res.status(404).json({
+                    success: false,
+                    error: 'No transaction found',
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: transaction,
+            count: transaction.length
+        })
+    } catch(err) {
+        return res.status(500).json({
+            success: false,
+            error: 'server error'
+        })
+    }
+}
+
+
 exports.addTransaction = async(req, res, next) => {
     try{
             const { remark, amount } = req.body;
@@ -45,4 +71,62 @@ exports.addTransaction = async(req, res, next) => {
             })
         }
     }
+}
+
+exports.deleteTransaction = async(req, res, next) => {
+
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+    
+
+    if(!transaction) {
+        return res.status(404).json({
+            success: false,
+            error: 'No transaction found'
+        })
+    }
+
+    await transaction.remove();
+
+    return res.status(200).json({
+        success: true,
+        data: {}
+    })
+
+}
+     catch(err) {
+        return res.status(500).json({
+        success: false,
+        error: 'server error'
+    })
+}
+}
+
+exports.updateTransaction = async (req, res, next) => {
+
+    
+        if(!req.body){
+            return res.status(400).send({
+                message: "Please fill all required field"
+              });
+        }
+
+    try {
+         const transaction = await Transaction.findByIdAndUpdate(req.params.id, {
+             amount: req.body.amount,
+             remark: req.body.remark
+         }, {new: true})
+
+         return res.status(201).json({
+            success: true,
+            data: transaction
+        })
+
+    } catch(err) {
+            return res.status(500).json({
+            success: false,
+            error: 'server error'
+            })
+    }
+
 }
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router();
-const { addTransaction, getTransactions } = require('../controllers/transactions');
+const { addTransaction, getTransactions, deleteTransaction, getTransactionsById, updateTransaction } = require('../controllers/transactions');
 
 router
     .route('/')
     .get(getTransactions)
     .post(addTransaction)
+
+router
+    .route('/:id')
+    .delete(deleteTransaction)
+    .get(getTransactionsById)
+    .put(updateTransaction)
 
 module.exports = router;
